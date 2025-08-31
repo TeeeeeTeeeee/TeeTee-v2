a0d283f0343ab07f83ab99ae0e9db63346722412
JingYuan <jingyuan0926@gmail.com>
2025-09-01 05:43:59 +0800

    Added host able to save hosted shard and profit sharing

--- smartcontract/contracts/creditUse.sol
+++ smartcontract/contracts/creditUse.sol
@@ -7,6 +7,17 @@ contract CreditUse {
 
     mapping(address => uint256) public userCredits;
 
+    struct HostedLLMEntry {
+        address host1;      
+        address host2;      
+        string shardUrl1;   
+        string shardUrl2;   
+        string modelName;
+        uint256 poolBalance;
+    }
+
+    HostedLLMEntry[] public hostedLLMs;
+
     function buyCredits() external payable {
     // Allow paying for 1 or more bundles at once
     require(msg.value % BUNDLE_PRICE == 0, "Send a multiple of 0.001 0G");
@@ -20,8 +31,12 @@ contract CreditUse {
         return userCredits[user];
     }
 
-    function usePrompt() external {
-        require(userCredits[msg.sender] > 0, "No prompt credits");
-        userCredits[msg.sender] -= 1;
-    }
+    function usePrompt(uint256 llmId) external {
+    require(userCredits[msg.sender] > 0, "No prompt credits");
+    require(llmId < hostedLLMs.length, "Invalid LLM");
+
+    userCredits[msg.sender] -= 1;              
+    hostedLLMs[llmId].poolBalance += 1;        
+}
+
 }
