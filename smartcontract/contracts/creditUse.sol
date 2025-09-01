89cd548bc8e477b94ed3673a9446e6d8812355ce
JingYuan <jingyuan0926@gmail.com>
2025-09-02 00:30:36 +0800

    Added only owner modifier

--- smartcontract/contracts/creditUse.sol
+++ smartcontract/contracts/creditUse.sol
@@ -8,35 +8,43 @@ contract CreditUse {
     mapping(address => uint256) public userCredits;
 
     struct HostedLLMEntry {
-        address host1;      
-        address host2;      
-        string shardUrl1;   
-        string shardUrl2;   
+        address host1;
+        address host2;
+        string shardUrl1;
+        string shardUrl2;
         string modelName;
         uint256 poolBalance;
     }
 
     HostedLLMEntry[] public hostedLLMs;
 
-    function buyCredits() external payable {
-    // Allow paying for 1 or more bundles at once
-    require(msg.value % BUNDLE_PRICE == 0, "Send a multiple of 0.001 0G");
-    uint256 bundles = msg.value / BUNDLE_PRICE;
-    require(bundles > 0, "No value sent");
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
 
-    userCredits[msg.sender] += bundles * BUNDLE_AMOUNT;
-}
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function buyCredits() external payable {
+        // Allow paying for 1 or more bundles at once
+        require(msg.value % BUNDLE_PRICE == 0, "Send a multiple of 0.001 0G");
+        uint256 bundles = msg.value / BUNDLE_PRICE;
+        require(bundles > 0, "No value sent");
+
+        userCredits[msg.sender] += bundles * BUNDLE_AMOUNT;
+    }
 
     function checkUserCredits(address user) public view returns (uint256) {
         return userCredits[user];
     }
 
     function usePrompt(uint256 llmId) external {
-    require(userCredits[msg.sender] > 0, "No prompt credits");
-    require(llmId < hostedLLMs.length, "Invalid LLM");
+        require(userCredits[msg.sender] > 0, "No prompt credits");
+        require(llmId < hostedLLMs.length, "Invalid LLM");
 
-    userCredits[msg.sender] -= 1;              
-    hostedLLMs[llmId].poolBalance += 1;        
-}
-
+        userCredits[msg.sender] -= 1;
+        hostedLLMs[llmId].poolBalance += 1;
+    }
 }
